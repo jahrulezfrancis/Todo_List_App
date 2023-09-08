7f94fc9c94e9a1a02aca34facb72c9a999d754b5
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-09-08 01:39:18 +0100

    Intial commit

---
+++ my_app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Center(
+      child: Text(
+    'Text goes here, straight outta campton',
+    textDirection: TextDirection.ltr,
+  )));
+}
