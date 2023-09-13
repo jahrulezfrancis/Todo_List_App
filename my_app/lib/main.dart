720dd82513107c32757b574ab55d904dfb65ad31
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-09-13 08:50:55 +0100

    Completing todo list

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,46 +1,87 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const App());
-
-class App extends StatelessWidget {
-  const App({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
+void main() => runApp(const MaterialApp(home: HomePage()));
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  String _inputText = '';
+  final List<String> _todos = [];
+
+  void _addTodo() {
+    showDialog(
+        context: context,
+        builder: ((BuildContext context) {
+          String newTodo = '';
+          return AlertDialog(
+            title: const Text('Create new task'),
+            content: TextField(
+                onChanged: (value) => {
+                      newTodo = value,
+                    }),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () => {Navigator.of(context).pop()},
+                  child: const Text('Cancel')),
+              TextButton(
+                onPressed: () {
+                  setState(
+                    () {
+                      _todos.add(newTodo);
+                    },
+                  );
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Create'),
+              ),
+            ],
+          );
+        }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Powerful To do List App')),
-      body: Center(
-          child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: TextField(
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter Catch Phrase'),
-                onChanged: (value) {
+      appBar: AppBar(
+        title: const Text('Todo App'),
+      ),
+      body: ListView.builder(
+        itemCount: _todos.length,
+        itemBuilder: (context, index) {
+          final todo = _todos[index];
+          return ListTile(
+              mouseCursor: SystemMouseCursors.click,
+              title: GestureDetector(
+                child: Text(
+                  todo,
+                  style: TextStyle(
+                      decoration: todo.startsWith('-')
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none),
+                ),
+                onTap: () {
                   setState(() {
-                    _inputText = value;
+                    if (todo.startsWith('-')) {
+                      _todos[index] = todo.substring(2);
+                    } else {
+                      _todos[index] = '- $todo';
+                    }
                   });
                 },
-              ))),
-      bottomSheet: Container(
-        alignment: Alignment.center,
-        height: 50,
-        child: Text('You just entered $_inputText as your catch phrase.'),
+                onDoubleTap: () {
+                  setState(() {
+                    _todos.removeAt(index);
+                  });
+                },
+              ));
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTodo,
+        child: const Icon(Icons.add),
       ),
     );
   }
